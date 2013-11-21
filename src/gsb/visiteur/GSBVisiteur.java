dee8e55bdaf370b32561b16ac05e86ed966dffaa
btssio <btssio@btssio-VirtualBox>
2013-11-21 16:06:59 +0100

    Nouveau projet aballenghien

---
+++ src/gsb/visiteur/GSBVisiteur.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gsb.visiteur;
+
+/**
+ *
+ * @author btssio
+ */
+public class GSBVisiteur {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
